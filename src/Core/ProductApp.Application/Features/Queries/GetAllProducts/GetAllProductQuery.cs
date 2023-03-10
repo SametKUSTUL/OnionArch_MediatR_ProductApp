7208702504616c4d8b9db03716dd3f06066a0d68
Samet KUSTUL <sametkustul@gmail.com>
2023-03-10 15:37:02 +0300

    Add project files.

---
+++ src/Core/ProductApp.Application/Features/Queries/GetAllProducts/GetAllProductQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Application.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Features.Queries.GetAllProducts
+{
+    public class GetAllProductQuery:IRequest<ServiceResponse<List<ProductViewDto>>>
+    {
+
+        public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQuery, ServiceResponse<List<ProductViewDto>>>
+        {
+            private readonly IProductRepository _repository;
+            private readonly IMapper _mapper;
+
+            public GetAllProductQueryHandler(IProductRepository repository,IMapper mapper)
+            {
+                _repository = repository;
+                _mapper = mapper;
+            }
+            public async Task<ServiceResponse<List<ProductViewDto>>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+            {
+                var product=await _repository.GetAllAsync();
+
+                var viewModel=_mapper.Map<List<ProductViewDto>>(product);
+
+                return new ServiceResponse<List<ProductViewDto>> (viewModel);
+            }
+        }
+    }
+}
---
+++ src/Core/ProductApp.Application/Features/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Application.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Features.Queries.GetProductById
+{
+    public class GetProductByIdQuery:IRequest<ServiceResponse<ProductViewDto>>
+    {
+        public Guid Id { get; set; }
+        public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ServiceResponse<ProductViewDto>>
+        {
+            IProductRepository _repository;
+            IMapper _mapper;
+
+            public GetProductByIdQueryHandler(IProductRepository repository, IMapper mapper)
+            {
+                _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+                _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            }
+
+            public async Task<ServiceResponse<ProductViewDto>> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+            {
+                var product = _repository.GetByIdAsync(request.Id);
+                var responseModel=_mapper.Map<ProductViewDto>(product.Result);
+                return new ServiceResponse<ProductViewDto>(responseModel);
+            }
+        }
+
+    }
+}
